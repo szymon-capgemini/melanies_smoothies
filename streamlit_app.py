c9d069e8cee1b032969a6432577727161e1ffacf
szymon-capgemini <szymon.adamik@capgemini.com>
2025-05-21 12:59:33 +0200

    Update streamlit_app.py
    remove after debugging message

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,6 @@ if ingredients_list:
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.write('after')
         
         st.subheader(fruit_chosen + ' nutritional info')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
